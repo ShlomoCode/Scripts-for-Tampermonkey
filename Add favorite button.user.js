1ac5b723e160cd2af4e06e506d0b5e1f209ac5bb
Shlomo <78599753+ShlomoCode@users.noreply.github.com>
2022-02-28 11:32:16 +0200

    Create Add favorite button.user.js

---
+++ Add favorite button.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         הוספת כפתור מועדפים
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @match        https://tchumim.com/*
+// @match        https://mitmachim.top/*
+// @icon https://mitmachim.top/assets/uploads/profile/566-profileavatar-1574614377858.jpeg
+// ==/UserScript==
+if(app.user.uid != 0){
+    var nav = document.getElementById("main-nav");
+    var li = document.querySelector("#main-nav > li:nth-child(4)");
+    var favorites = document.createElement("li");
+
+    favorites.innerHTML = '<a class="navigation-link" title="מועדפים" data-original-title="מועדפים" href="/uid/' + app.user.uid + '/bookmarks"><i class="fa fa-heart"></i><span class="visible-xs-inline">מועדפים</span></a>';
+
+    nav.insertBefore(favorites, li);
+}
